ae1cc76ae3debac903785755af120d68ab2cbc88
aljaz <aljaz.romih@gmail.com>
2016-11-08 10:39:45 +0100

    update

--- Dobre misli/Default.aspx.cs
+++ Dobre misli/Default.aspx.cs
@@ -9,14 +9,23 @@ namespace Dobre_misli
 {
     public partial class Default : System.Web.UI.Page
     {
+        static List<Label> labels = new List<Label>();
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if (IsPostBack)
+            {
+                foreach (var label in labels)
+                    misli.Controls.Add(label);
+            }
         }
 
         protected void dodajMisel_Click(object sender, EventArgs e)
         {
-                    
+            Label l = new Label();
+            l.Text = '"' + misel.Text + '"';
+            l.Style.Add("display", "block");
+            labels.Add(l);
+            misli.Controls.Add(l);
         }
     }
     
